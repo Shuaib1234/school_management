56290533546c771d65d1f4aec4f51e5c6d843a6f
Ali <alimohamedreddit@gmail.com>
2022-06-11 17:23:56 +0200

    Created Student entity using builder pattern

---
+++ src/main/java/za/ac/cput/factory/StudentFactory.java
@@ -0,0 +1,11 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Name;
+import za.ac.cput.domain.Student;
+
+public class StudentFactory {
+
+    public static Student createStudent(String studentId, String email, Name name){
+       return null;
+    }
+}
